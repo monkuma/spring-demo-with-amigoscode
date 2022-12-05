33c5f882e32516756b88d1f0c2a918eea4f1dd07
monkuma <kumamoto.jobs@gmail.com>
2022-12-05 22:25:04 +0900

    add docker-compose, postgres and CRUD func

--- src/main/java/com/kumacode/Main.java
+++ src/main/java/com/kumacode/Main.java
@@ -2,32 +2,57 @@ package com.kumacode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.SimpleTimeZone;
 
 // https://youtu.be/-mwpoE0x0JQ?t=3605
 @SpringBootApplication
 @RestController
+@RequestMapping("/api/v1/customers")
 public class Main {
+
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String[] args) {
+
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/greet")
-    public GreetResponse greet(){
-        GreetResponse response = new GreetResponse("Good Morning",
-                List.of("Java", "Javascript", "Golang"),
-                new Person("Alex", 30, 40_000));
-
-        return response;
+    @GetMapping()
+    public List<Customer> getCustomer(){
+        return customerRepository.findAll();
     }
 
-    record  Person(String name, int age, double cash){
+    record NewCustomerRequest(String name, String email, Integer age){
+
     }
-    record GreetResponse(String greet,
-                         List<String> favProgrammingLanguages,
-                         Person person){
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest newCustomerRequest){
+        Customer customer = new Customer();
+        customer.setName(newCustomerRequest.name());
+        customer.setEmail(newCustomerRequest.email());
+        customer.setAge(newCustomerRequest.age());
+        customerRepository.save(customer);
     }
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id){
+        customerRepository.deleteById(id);
+    }
+
+    @PostMapping("{customerId}")
+    public void updateCustomer(@RequestBody NewCustomerRequest newCustomerRequest, @PathVariable("customerId") Integer id){
+        Customer customer = customerRepository.findById(id).get();
+        customer.setName(newCustomerRequest.name());
+        customer.setEmail(newCustomerRequest.email());
+        customer.setAge(newCustomerRequest.age());
+        customerRepository.save(customer);
+    }
+
 }
